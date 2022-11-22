1471cde366b0ba34eeb93e51ca0b061d7f5e253b
gerardo mexicano <germx06@gmail.com>
2022-11-22 17:28:14 -0600

    Excel

--- ExportExcel.py
+++ ExportExcel.py
@@ -21,15 +21,16 @@ class ExportExcel():
             c_solicitud=dato[i][5]
             c_empresa=dato[i][6]
             c_comentario=dato[i][7]"""
-        fecha = 'pastel'
+        fecha = str(strftime('%d-%m-%y_%H-%M-%S'))
         #datos = {Clienteref,c_alta,c_folio,c_cope,c_solicitud,c_empresa,c_comentario}
         #datos = {'clienteref':Clienteref,'c_alta':c_alta,'c_folio':c_folio,'c_cope':c_cope,'c_solicitud':c_solicitud,'c_empresa':c_empresa,'c_comentario':c_comentario}
         df = pd.DataFrame(datos,columns=['ID','CHAT_NUM','Alta','Folio','COPE','Solicitud','Empresa','Comentario','comentario' ])
-        df.to_excel(('DATOS.xlsx'))
+        df.to_excel((f'DATOS {fecha} .xlsx'))
 
 if __name__ == "__main__":
     print("inicio")
     c=ExportExcel()
+    
     c.exportar()
     print("fin")
 
